36706060e57df316570bc72ee64a1f86cd216bf7
Sujit Suman Sahoo <sujit05061994@gmail.com>
2024-06-11 04:41:27 +0530

    Fixing Logo and Icon issue for the deployed vercel Link

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import "./header.css";
 const Header = () => {
   return (
     <div>
-      <img className="logo" src="./src/assets/EZ Works Blue/EZ Works Blue@2x.png" alt="logo" />
+      <img className="logo" src="/assets/EZ Works Blue/EZ Works Blue@2x.png" alt="logo" />
       <h1 className="heading">Suite Of Business Support Services</h1>
       <p className="para">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
